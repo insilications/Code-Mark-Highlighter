d8f7a461310324529759dce4768aa84fc2a720e8
Francisco Boni <boboniboni@gmail.com>
2026-08-13 05:52:12 -0300

    Small fixes & housekeeping.

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -27,9 +27,7 @@ export default defineConfig([
     logLevel: "debug",
     output: {
       dir: "out",
-      // Modern browsers natively support ES Modules
       format: "esm",
-      // format: "iife",
       sourcemap: true,
       minify: false,
     },
@@ -83,63 +81,3 @@ export default defineConfig([
     // ],
   },
 ]);
-
-// export default defineConfig({
-//   input: ["src/extension.ts", "src/mainWebView.ts"],
-//   output: {
-//     dir: "out",
-//     // VS Code extensions currently require CommonJS
-//     format: "commonjs",
-//     // Crucial for breakpoints/debugging in VS Code
-//     sourcemap: true,
-//     minify: true,
-//     cleanDir: true,
-//   },
-
-//   // Treat Node built-ins (like fs, path) natively rather than bundling them
-//   platform: "node",
-//   tsconfig: "./tsconfig.json",
-//   // DO NOT bundle the 'vscode' module, as the VS Code host provides it at runtime
-//   external: ["vscode"],
-// });
-
-// export default defineConfig(
-//   {
-//     // Point this to your extension's main entry file
-//     input: "src/extension.ts",
-
-//     output: {
-//       // The standard VS Code generator uses 'out' or 'dist'
-//       file: "out/extension.js",
-//       format: "commonjs", // VS Code extensions currently require CommonJS
-//       sourcemap: true, // Crucial for breakpoints/debugging in VS Code
-//       minify: false, // Set to true for production builds
-//       cleanDir: true,
-//     },
-
-//     // Treat Node built-ins (like fs, path) natively rather than bundling them
-//     platform: "node",
-//     tsconfig: "./tsconfig.json",
-//     // DO NOT bundle the 'vscode' module, as the VS Code host provides it at runtime
-//     external: ["vscode"],
-//   },
-//   {
-//     // Point this to your extension's main entry file
-//     input: "src/mainWebView.ts",
-
-//     output: {
-//       // The standard VS Code generator uses 'out' or 'dist'
-//       file: "out/mainWebView.js",
-//       format: "commonjs", // VS Code extensions currently require CommonJS
-//       sourcemap: true, // Crucial for breakpoints/debugging in VS Code
-//       minify: false, // Set to true for production builds
-//       cleanDir: true,
-//     },
-
-//     // Treat Node built-ins (like fs, path) natively rather than bundling them
-//     platform: "node",
-//     tsconfig: "./tsconfig.json",
-//     // DO NOT bundle the 'vscode' module, as the VS Code host provides it at runtime
-//     external: ["vscode"],
-//   },
-// );
